41c5067c3a3008560c5f819d766a582e74bc1bfd
Cody Derbyshire <derbyshire@outlook.co.nz>
2024-01-31 11:13:10 +1300

    made hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Header from './components/Header';
+
+const me = './src/assets/images/me.jpg';
+const alsome = './src/assets/images/also-me.jpg';
+const stillme = './src/assets/images/still-me.jpg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='overlay'></div>
+      <Header />
+      <div className='main'>
+        <div className='hero'>
+          <p className='hero-text'>
+            WEB DEVELOPER <br />+ DIGITAL DESIGNER
+          </p>
+          <h1 className='intro'>
+            CODY <br /> DERBYSHIRE
+          </h1>
+          <img
+            className='hero-img'
+            src={stillme}
+            alt='cody derbyshire hero image'
+          />
+          <h1 className=''>DESIGNER + DEVELOPER.</h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+    <>
+      <div className='header'>
+        <p className='header-name'>Cody Derbyshire</p>
+      </div>
+    </>
+  );
+};
+
+export default Header;
